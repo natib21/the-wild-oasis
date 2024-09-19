15e207ddf4e73f2b2902518208170d2147cff950
nathnael zelalem <nathnaelzelalem@gmail.com>
2024-09-19 06:20:05 -0700

    add Styled Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,30 @@
+import styled from "styled-components"
+import GlobalStyle from "./styles/GlobalStyles"
+import Button from "./ui/Button"
+import Input from "./ui/Input"
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight:600;
+  text-transform: capitalize;
+  color: red;
+  background-color: yellow;
+`
+
+
+const StyledApp = styled.div`
+  background-color: orangered;
+  padding: 20px;
+`
 const App = () => {
-  return (
-    <div>Hello World</div>
+  return (<>
+  <GlobalStyle />
+  <StyledApp>
+
+   <H1>The Wild Oasis </H1>
+   <Button onClick={()=>alert("Hello ")}>Check in</Button>
+   <Input type="number" placeholder="Number of Guest" />
+  </StyledApp>
+  </>
   )
 }
 export default App
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -47,3 +47,20 @@ const variations = {
     }
   `,
 };
+
+const Button = styled.button`
+  background-color: var(--color-brand-600);
+  color:var(--color-brand-50);
+  border:none;
+  border-radius: var(--border-radius-sm);
+  font-size: 1.4rem;
+  padding: 1.2rem 1.6rem;
+  font-weight: 500;
+  box-shadow: var(--shadow-sm);
+
+  &:hover {
+    background-color: var(--color-brand-700);
+  }
+`
+
+export default Button
