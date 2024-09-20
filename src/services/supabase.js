fd8b3a3e17033835f096726353cf1916d42a25a8
Nathnael Zelalem <nathnaelzelalem@gmail.com>
2024-09-20 23:36:56 +0300

    connect react app with supabase database

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+
+import { createClient } from '@supabase/supabase-js'
+const supabaseUrl = 'https://qfzoqiejrlvjehltbnks.supabase.co'
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFmem9xaWVqcmx2amVobHRibmtzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjY4NTgxOTgsImV4cCI6MjA0MjQzNDE5OH0.RI2j6_qBlFGI05sFrVrbfjCDx6i287b_29OI3xkLKUE"
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+export default supabase;
