abce87bcc186f90f53d6cb0d3791eede6158dad5
nathnael zelalem <nathnaelzelalem@gmail.com>
2024-09-23 06:27:53 -0700

    Displaying Toasts

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import { formatCurrency } from "../../utils/helpers";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { deleteCabin } from "../../services/apiCabin";
+import toast from "react-hot-toast";
 const TableRow = styled.div`
   display: grid;
   grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
@@ -54,13 +55,13 @@ const CabinRow = ({ cabin }) => {
   const { isDeleting, mutate } = useMutation({
     mutationFn: deleteCabin,
     onSuccess: () => {
-      alert("Cabin Successfully deleted");
+      toast.success("Cabin Successfully deleted");
       queryClient.invalidateQueries({
         queryKey: ["cabins"],
       });
     },
     onError: (err) => {
-      alert(err.message);
+      toast.err(err.message);
     },
   });
   return (
